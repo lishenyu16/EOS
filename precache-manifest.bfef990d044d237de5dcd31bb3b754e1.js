dc62e1528c59819f31748943ca97fa3b965892bd
Shenyu <lishenyu16@gmail.com>
2019-06-10 21:00:28 -0700

    Updates

---
+++ precache-manifest.bfef990d044d237de5dcd31bb3b754e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7df99c810a08f7a919a0086b78c0ef5",
+    "url": "/EOS/index.html"
+  },
+  {
+    "revision": "39aa18b1fb717f72ea28",
+    "url": "/EOS/static/css/2.2109d0bc.chunk.css"
+  },
+  {
+    "revision": "eb7e4be485a22e183044",
+    "url": "/EOS/static/css/main.7ad2e692.chunk.css"
+  },
+  {
+    "revision": "39aa18b1fb717f72ea28",
+    "url": "/EOS/static/js/2.320fb755.chunk.js"
+  },
+  {
+    "revision": "eb7e4be485a22e183044",
+    "url": "/EOS/static/js/main.525e2d61.chunk.js"
+  },
+  {
+    "revision": "8cc7236dea193996081b",
+    "url": "/EOS/static/js/runtime~main.cacbd93d.js"
+  },
+  {
+    "revision": "059574189c336b926d2c7ec7af4c6399",
+    "url": "/EOS/static/media/Background.05957418.png"
+  },
+  {
+    "revision": "5280bdc6f54259bb17bf9a649c20c3f3",
+    "url": "/EOS/static/media/Logo_battle.5280bdc6.png"
+  }
+]);
